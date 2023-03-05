86f5f7f4c2cdafcf84328aa311e1b8853264d530
Naraveni Akhil <naraveniakhil@email.com>
2023-03-05 16:41:26 +0530

    added onrender host URL

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -31,7 +31,7 @@ const Signup =(props)=> {
             setValid(true)
         }
         else{
-            axios.post('http://localhost:5000/api/v1/register', data)
+            axios.post('https://contactmanager-22.onrender.com/api/v1/register', data)
             
             .then(result=>{
                 console.log(data)
